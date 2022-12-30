a91c294785d74b3f4eaffc51d1ff5a0dbf8db3e8
Tim Ittermann <tim.ittermann@gmail.com>
2022-12-30 16:40:24 +0100

    Remove comments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,7 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import { Calendar } from "../components/index/Calendar";
 
-// from-[#2e026d] to-[#15162c]
-
 const Home: NextPage = () => {
-  //const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
-
   return (
     <>
       <Head>
