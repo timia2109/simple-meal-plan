16fcdd67f13551539bbd9515f4e1a60a004a69fa
Tim Ittermann <tim.ittermann@gmail.com>
2023-01-13 23:53:19 +0100

    add hack to getSession

--- src/server/common/get-server-auth-session.ts
+++ src/server/common/get-server-auth-session.ts
@@ -3,6 +3,9 @@ import { unstable_getServerSession } from "next-auth";
 
 import { authOptions } from "../../pages/api/auth/[...nextauth]";
 
+const desiredName = "next-auth.session-token";
+const authType = "Bearer ";
+
 /**
  * Wrapper for unstable_getServerSession https://next-auth.js.org/configuration/nextjs
  * See example usage in trpc createContext or the restricted API route
@@ -11,5 +14,12 @@ export const getServerAuthSession = async (ctx: {
   req: GetServerSidePropsContext["req"];
   res: GetServerSidePropsContext["res"];
 }) => {
+  // Hack to also allow Bearer Tokens
+  const authHeader = ctx.req.headers["authorization"];
+  if (authHeader !== undefined && authHeader?.indexOf(authType) !== -1) {
+    const token = authHeader?.substring(authType.length);
+    ctx.req.cookies[desiredName] = token;
+  }
+
   return await unstable_getServerSession(ctx.req, ctx.res, authOptions);
 };
