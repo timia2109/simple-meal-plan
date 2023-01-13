f581a13dc1067a9c2a5dcc0ed14af813038170ba
Tim Ittermann <tim.ittermann@gmail.com>
2023-01-13 14:36:00 +0100

    Add google auth to env vars

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -8,6 +8,9 @@ import { z } from "zod";
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
+  GOOGLE_CLIENT_SECRET: z.string(),
+  GOOGLE_CLIENT_ID: z.string(),
+  INVITATION_VALIDITY: z.string().default("P30D"),
 });
 
 /**
