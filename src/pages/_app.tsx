fcefb88b09472fbbe9d71c213601e46b449a9e4d
Tim Ittermann <tim.ittermann@gmail.com>
2023-01-13 23:52:54 +0100

    integrate i18next to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@ import { type AppType } from "next/app";
 
 import { trpc } from "../utils/trpc";
 
+import { appWithTranslation } from "next-i18next";
+import i18nConfig from "../../next-i18next.config";
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
 
-export default trpc.withTRPC(MyApp);
+export default trpc.withTRPC(appWithTranslation(MyApp, i18nConfig));
