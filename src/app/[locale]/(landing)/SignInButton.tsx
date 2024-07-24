b4dd166b7bf3ade651782d2814ad08b9795d5e2e
Tim Ittermann <tim.ittermann@gmail.com>
2024-07-24 20:02:00 +0200

    Add first Page

---
+++ src/app/[locale]/(landing)/SignInButton.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { signIn } from "next-auth/react";
+import type { FC } from "react";
+
+export const SignInButton: FC = () => {
+  return (
+    <button
+      className="btn-outline-primary btn btn-lg bg-gradient-to-r from-pink-500 to-purple-600 shadow-2xl"
+      onClick={() => signIn()}
+    >
+      <span className="px-16 text-xl font-semibold text-purple-950">
+        Sign In
+      </span>
+    </button>
+  );
+};
---
+++ src/app/[locale]/(landing)/page.tsx
@@ -0,0 +1,35 @@
+import { useTranslations } from "next-intl";
+import { SignInButton } from "./SignInButton";
+
+export default function LandingPage() {
+  const t = useTranslations("Landing");
+
+  return (
+    <div className="p-12">
+      <div className="fw-bolder flex flex-col items-center justify-center gap-16">
+        <div className="flex flex-col items-center justify-center gap-8 ">
+          <h1 className="text-6xl">
+            INDEX Text
+            <span className="bg-gradient-to-r from-pink-500 to-purple-600 bg-clip-text font-semibold text-transparent">
+              {" "}
+              {t("title")}
+            </span>
+          </h1>
+
+          <p className="text-center text-xl md:w-1/2">Main Text</p>
+        </div>
+        <SignInButton />
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
+          <div className="card w-96 bg-base-300 shadow-2xl">
+            <div className="card-body">
+              <h2 className="card-title flex justify-center">
+                Feature Heading
+              </h2>
+              <p>Feature Box</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
