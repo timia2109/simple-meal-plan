1baad104c98dd4b00d8cd07a5219976b5393c878
Tim Ittermann <tim.ittermann@gmail.com>
2024-08-02 15:35:41 +0200

    chore: Add updateMealEntry action for updating meal entries

---
+++ src/actions/updateMealEntry.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import { getMealPlan } from "@/dal/mealPlans/getMealPlan";
+import { submitMealEntry } from "@/dal/mealPlans/submitMealEntry";
+import { getUserId } from "@/functions/user/getUserId";
+import { z } from "zod";
+import { zfd } from "zod-form-data";
+
+const schema = zfd.formData({
+  date: z.string().datetime(),
+  mealPlanId: z.string(),
+  meal: z.string(),
+});
+
+export async function updateMealEntry(formData: FormData) {
+  const data = schema.safeParse(formData);
+  if (!data.success) {
+    return {
+      message: data.error.message,
+    };
+  }
+
+  const userId = await getUserId();
+  if (userId == null)
+    return {
+      message: "Login expected",
+    };
+
+  const { date, meal, mealPlanId } = data.data;
+
+  const mealPlan = await getMealPlan(userId, mealPlanId);
+  if (mealPlan == null)
+    return {
+      message: "MealPlan not found",
+    };
+
+  await submitMealEntry({
+    mealPlanId,
+    date: new Date(date),
+    meal,
+  });
+}
