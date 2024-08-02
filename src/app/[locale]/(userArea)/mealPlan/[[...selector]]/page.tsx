a7c5ec62589a20116ad0d59d3e4866d5a712673a
Tim Ittermann <tim.ittermann@gmail.com>
2024-08-02 15:35:47 +0200

    chore: Refactor meal plan page component

---
+++ src/app/[locale]/(userArea)/mealPlan/[[...selector]]/page.tsx
@@ -0,0 +1,27 @@
+import { MealPlanContainer } from "@/components/mealPlan/MealPlanContainer";
+import { getMealPlan } from "@/dal/mealPlans/getMealPlan";
+import { createKeyDate } from "@/functions/dateTime/createKeyDate";
+import { getUserId } from "@/functions/user/getUserId";
+import { notFound } from "next/navigation";
+
+type Props = {
+  params: {
+    selector: string[];
+  };
+};
+
+export default async function MealPlanPage({ params }: Props) {
+  const user = await getUserId(true);
+  const [mealPlanId, year, month] = params.selector ?? [];
+
+  const mealPlan = await getMealPlan(user, mealPlanId ?? null);
+  if (mealPlan == null) notFound();
+
+  const keyDate = createKeyDate(year, month);
+
+  return (
+    <div className="container mx-auto">
+      <MealPlanContainer keyDate={keyDate} mealPlan={mealPlan} />;
+    </div>
+  );
+}
