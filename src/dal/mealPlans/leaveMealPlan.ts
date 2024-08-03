3d369af50e9a3de90d8bd917a1acc9b904e1632b
Tim Ittermann <tim.ittermann@gmail.com>
2024-08-03 17:38:08 +0200

    feat: optimize leave meal plan

--- src/dal/mealPlans/leaveMealPlan.ts
+++ src/dal/mealPlans/leaveMealPlan.ts
@@ -1,37 +1,45 @@
 import { prisma } from "@/server/db/client";
 
-type Props = {
-  userId: string;
-  mealPlanId: string;
-};
-
 /**
- * Removes a User from a meal plan
+ * Remove a user from a meal plan and delete the meal plan if there are no participants left
+ * @param mealPlanId Meal Plan Id
+ * @param userId User Id
+ * @param force Should the user be removed from the meal plan even if it's the default
  */
-export async function leaveMealPlan({ mealPlanId, userId }: Props) {
-  // Must exist, the guard will care
-  await prisma.mealPlanAssignment.delete({
-    where: {
-      mealPlanId_userId: {
-        mealPlanId,
-        userId,
-      },
-    },
-  });
-
-  // Check if there is any remaining member
-  const assignmentCount = await prisma.mealPlanAssignment.count({
-    where: {
-      mealPlanId,
-    },
-  });
-
-  // Delete if this list has no participent
-  if (assignmentCount === 0) {
-    await prisma.mealPlan.delete({
+export async function leaveMealPlan(
+  mealPlanId: string,
+  userId: string,
+  force = false
+) {
+  await prisma.$transaction(async (tx) => {
+    const deletedElement = await tx.mealPlanAssignment.delete({
       where: {
-        id: mealPlanId,
+        mealPlanId_userId: {
+          mealPlanId,
+          userId,
+        },
       },
     });
-  }
+
+    // Abort if it's the default
+    if (deletedElement.userDefault && !force) {
+      throw new Error("Cannot leave default meal plan");
+    }
+
+    // Check if there is any remaining member
+    const assignmentCount = await tx.mealPlanAssignment.count({
+      where: {
+        mealPlanId,
+      },
+    });
+
+    // Delete if this list has no participants
+    if (assignmentCount === 0) {
+      await tx.mealPlan.delete({
+        where: {
+          id: mealPlanId,
+        },
+      });
+    }
+  });
 }
