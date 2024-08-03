4cdb3c132baf069c9f6fd47dc84ece4f60f8af43
Tim Ittermann <tim.ittermann@gmail.com>
2024-08-03 15:52:47 +0200

    feat: Optimize Manage page

--- src/app/[locale]/(userArea)/manage/page.tsx
+++ src/app/[locale]/(userArea)/manage/page.tsx
@@ -1,7 +1,7 @@
+import { MealPlanEntry } from "@/components/mealPlan/MealPlanEntry";
 import { getMealPlans } from "@/dal/mealPlans/getMealPlans";
 import { getUserId } from "@/functions/user/getUserId";
 import { getScopedI18n } from "@/locales/server";
-import { MealPlanEntry } from "./MealPlanEntry";
 
 export default async function ManageMealPlansPage() {
   const userId = await getUserId(true);
@@ -10,13 +10,16 @@ export default async function ManageMealPlansPage() {
 
   return (
     <div className="container mx-auto">
-      <h1 className="text-3xl font-extrabold">{t("manage")}</h1>
+      <h1 className="mb-1 text-3xl font-extrabold">{t("manage")}</h1>
+      <title>{t("manage")}</title>
       <div>
         {mealPlanAssignments.map((assignment) => (
           <MealPlanEntry
             key={assignment.mealPlanId}
-            assignment={assignment}
+            mealPlanAssignment={assignment}
             mealPlan={assignment.mealPlan}
+            withActions
+            withUsers
           />
         ))}
       </div>
