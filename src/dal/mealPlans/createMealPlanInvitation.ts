736c0e48e2b473eccd5c588465a48df4602f5d12
Tim Ittermann <tim.ittermann@gmail.com>
2024-08-03 18:24:58 +0200

    feat: fix invitation link creation

--- src/dal/mealPlans/createMealPlanInvitation.ts
+++ src/dal/mealPlans/createMealPlanInvitation.ts
@@ -1,4 +1,6 @@
+import { env } from "@/env/server.mjs";
 import { prisma } from "@/server/db/client";
+import { DateTime, Duration } from "luxon";
 import { generate } from "randomstring";
 
 type Props = {
@@ -7,13 +9,17 @@ type Props = {
 };
 
 const stringLength = 12;
+const invitationExpiresIn = Duration.fromISO(env.INVITATION_VALIDITY);
 
 /** Creates a invitation to a MealList */
 export function createMealPlanInvitation({ mealPlanId, userId }: Props) {
+  const expiration = DateTime.now().plus(invitationExpiresIn);
+
   return prisma.mealPlanInvite.create({
     data: {
       createdByUserId: userId,
       mealPlanId: mealPlanId,
+      expiresAt: expiration.toJSDate(),
       invitationCode: generate({
         length: stringLength,
         capitalization: "uppercase",
