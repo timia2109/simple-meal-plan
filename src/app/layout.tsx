bf412607ca3bb6859372c1615026ba4da8cefa74
Tim Ittermann <tim.ittermann@gmail.com>
2024-08-17 11:12:17 +0200

    refactor: Try do disable zooming on phones

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default async function LocaleLayout({
 
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
       </Head>
       <body className="flex h-screen flex-col justify-between bg-base-100">
